c290a4d4c496d33ebfd4a0149c239204d991643e
Dilip <dilipkumar4813@gmail.com>
2017-06-12 23:04:51 +0530

    Removal of unwanted files and optimization

--- app/src/main/java/iamdilipkumar/com/cloudjokes/MainActivity.java
+++ app/src/main/java/iamdilipkumar/com/cloudjokes/MainActivity.java
@@ -2,16 +2,15 @@ package iamdilipkumar.com.cloudjokes;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.iamdilipkumar.cloudjokes.backend.myApi.MyApi;
 
 import java.io.IOException;
@@ -23,43 +22,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+        Button btnJoke = (Button) findViewById(R.id.btn_joke);
+        btnJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new EndpointsAsyncTask().execute(MainActivity.this);
+            }
+        });
     }
 
-    private class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
         private MyApi myApiService = null;
         private Context context;
 
         @Override
-        protected String doInBackground(Pair<Context, String>... params) {
-            Log.d("test", "starting background task");
-            if (myApiService == null) {  // Only do this once
-                Log.d("test", "Service task not null");
-
+        protected String doInBackground(Context... params) {
+            if (myApiService == null) {
                 MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                         new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // - 10.0.2.2 is localhost's IP address in Android emulator
-                        // - turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
-                // end options for devappserver
+                        .setRootUrl("https://cloud-jokes.appspot.com/_ah/api/");
 
                 myApiService = builder.build();
             }
 
-            context = params[0].first;
-            String name = params[0].second;
+            context = params[0];
 
             try {
-                return myApiService.sayHi(name).execute().getData();
+                return myApiService.getJoke().execute().getJoke();
             } catch (IOException e) {
-                Log.d("error", e.getMessage().toString());
+                Log.d("error", e.getMessage());
                 return e.getMessage();
             }
         }
